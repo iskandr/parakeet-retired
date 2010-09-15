0c7cd41a1c29af18d77710d5ab8ff60ca56b2ac2
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2010-09-15 15:42:56 -0400

    Got rid of settings and build dir

--- _build/Init.ml
+++
@@ -1,9 +0,0 @@
-open HardwareInfo
-open LibPQ
-
-(**
- * Initializes the compiler.  Called once from Q/C.
- *)
-let compiler_init () =
-  cuda_init ();
-  hw_init ()
--- _build/SSA/SSA_Gates.ml
+++
@@ -1,5 +0,0 @@
-type if_gate = { 
-    if_output_ids : ID.t list;
-    true_ids : ID.t list; 
-    false_ids : ID.t list 
-}
