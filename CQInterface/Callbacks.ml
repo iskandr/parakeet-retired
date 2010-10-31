882a16408973aa2903e003da932cf2b585565288
Alex Rubinsteyn <alexr@cs.nyu.edu>
2010-10-31 17:01:17 -0400

    added local version of Callbacks.ml

---
+++ CQInterface/Callbacks.ml
@@ -0,0 +1,15 @@
+(* Place to collect all of the callbacks from C to OCaml so as to facilitate
+   easier builds. *)
+
+let _ = Callback.register "shape_create" Shape.create
+let _ = Callback.register "shape_rank" Shape.rank
+let _ = Callback.register "compiler_init" Init.compiler_init
+let _ = Callback.register "sizeof_dyn_type" DynType.sizeof
+let _ = Callback.register "ktypenum_to_ocaml_type" QType.ktypenum_to_ocaml_type
+let _ = Callback.register "dyn_type_to_ktypenum" QType.dyn_type_to_ktypenum
+let _ = Callback.register "gen_module_template" CodeTemplate.gen_module_template
+let _ = Callback.register "get_function_template"
+  CodeTemplate.get_function_template
+let _ = Callback.register "run_template" CodeTemplate.run_template
+let _ = Callback.register "c_compact" CodeTemplate.c_compact
+
