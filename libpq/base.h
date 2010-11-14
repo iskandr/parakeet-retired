5e78e041c1c884e305524f842bfd2e02be31a719
erik <erik@wurst.(none)>
2010-11-13 20:36:57 -0500

    fixed scalar c interface bug

--- libpq/base.h
+++ libpq/base.h
@@ -6,6 +6,8 @@ extern "C" {
 #endif
 
 #define THREADS_PER_LINEAR_BLOCK 256
+#define THREADS_PER_BLOCK 256
+#define THREADS_PER_DIM 16
 
 void check_err(int rslt, char *msg);
 void make_linear_grid(int num_blocks, int *gridX, int *gridY);
