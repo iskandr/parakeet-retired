3555ae16f7763a91c4c56cd2117c2644d609fa37
Alex Rubinsteyn <alexr@cs.nyu.edu>
2010-12-13 02:18:47 -0500

    renamed FindGenSet to FindBindingSet

---
+++ SSA_Analyses/FindBindingSet.ml
@@ -0,0 +1,17 @@
+open Base 
+open SSA 
+
+module Env  = struct
+  type t = ID.Set.t 
+  let init fundef = ID.Set.of_list (fundef.input_ids @ fundef.output_ids)  
+end
+module BindingSetAnalysis = struct
+  include SSA_Analysis.MkSimpleAnalysis(Env) 
+  let stmt env stmtNode info = match stmtNode.stmt with 
+    | Set (ids, _) -> Some (ID.Set.add_list ids env)
+    | _ -> failwith "not implemented"  
+end
+module BindingSetEval = SSA_Analysis.MkEvaluator(BindingSetAnalysis)
+
+let fundef_bindings fundef = BindingSetEval.eval_fundef fundef 
+let block_bindings block = BindingSetEval.eval_block ID.Set.empty block    
