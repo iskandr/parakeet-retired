a5dd30e7598e2f4a26e74cc89d316b3257c001d8
Alex Rubinsteyn <alexr@cs.nyu.edu>
2011-02-25 01:30:44 -0500

    a little work on CostModel

--- CostModel.ml
+++ CostModel.ml
@@ -8,21 +8,41 @@ module type COST_MODEL_PARAMS = sig
 end 
 
 module Mk(P : COST_MODEL_PARAMS) = struct
-  let rec map_cost ~fn ~closureArgs ~dataArgs  = 
-    let shapes = List.map (MemoryState.get_shape memState) dataArgs in
-    let maxShape = match Shape.max_shape_list shapes with 
+  
+  module G = GpuCost.Mk(struct let fnTable = fnTable end) 
+  
+   
+  let max_arg_shape args = 
+    let argShapes = List.map (fun (s,_,_) -> s) args in 
+    match Shape.max_shape_list shapes with 
       | Some maxShape -> maxShape 
-      | None -> failwith "max shape not found"
-    in
-    let gpuCost = 
-    let nestedCost =  
-    (* assume we process 100 elements per millisecond on the host, 
-       but also assume nested computations are free
-     *) 
-    assert (Shape.rank maxShape > 0); 
-    let runCost = (Shape.get maxShape 0) / 10 in 
-    CPU, 1 +  runCost  
-  and call_cost ~memState     
+      | None -> failwith "max shape not found" 
+  
+  (* split args into max dim, and peeled inner args *) 
+  let split_args args =
+    let maxShape = max_arg_shape args in
+    assert (Shape.rank maxShape > 0);
+    let peeler (shape,ty,onGpu) =
+      let shape' = 
+        if Shape.eq shape maxShape then Shape.peel_shape shape else shape
+      in 
+      shape', ty, onGpu
+    in  
+    Shape.get maxShape 0, List.map peeler args 
+  
+  let peel_args args = snd (split_args args)   
+  
+  let rec map_cost fn closureArgs args =  
+    let gpuCost = G.map fn closureArgShapes args in
+    let maxDim, nestedArgs = split_args args in   
+    let nestedCost = call_cost f (closureArgs @ nestedArgs) in   
+    let cpuCost = 1 + maxDim * nestedCost in 
+    if cpuCost < gpuCost then CPU, cpuCost 
+    else GPU, gpuCost  
+  
+  and call_cost fn args = 
+     
+    
   
   let array_op op argVals = match op, argVals with 
     | _ -> CPU, 0 (* don't run anything else on the host *)  
