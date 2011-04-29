29ad12ba1e30e9bc889b9e77dce1255251ba9989
nsw233 <nathanielweinman@aol.com>
2011-04-29 00:28:07 -0400

    need to define arrays as int32

--- Python/addn.py
+++ Python/addn.py
@@ -9,7 +9,8 @@ def mult9(x):
 def addxy(x,y):
   return x * y + x
 
-x = array([1,2,3,4,5,6,7,8,9,10,11,12,13,14])
-y = array([3,1,4,1,5,9,3,1,4,1,5,9,3,1])
-#print mult9(x)
+x = array([1,2,3,4,5,6,7,8,9,10,11,12,13,14],dtype = int32)
+y = array([3,1,4,1,5,9,3,1,4,1,5,9,3,1],dtype = int32)
+z = array([[1,2],[3,4]],dtype = int32)
+#print mult9(z)
 print addxy(x,y)
