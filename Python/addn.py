7eef41ef00c7b244b990f7f3a8dd98dae1da26b2
nsw233 <nathanielweinman@aol.com>
2011-05-03 00:14:05 -0400

    code is a bit neater

--- Python/addn.py
+++ Python/addn.py
@@ -7,7 +7,7 @@ def mult9(x):
   x = x + 4
   return x * 9
 
-#@GPU
+@GPU
 def addxy(x,y,z):
   return x * y + z
 #@GPU
@@ -23,5 +23,5 @@ y = array([3,1,4,1,5,9,3,1,4,1,5,9,3,1],dtype = int32)
 z = array([[1,2,3],[4,5,6]],dtype = int32)
 f = array([1.2,2.5],dtype = float64)
 print mult9(f)
-#print addxy(x,y,x)
+print addxy(x,y,x)
 #print whiletest(x)
