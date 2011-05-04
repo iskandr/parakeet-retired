92be49dbb0ea6cdc305e73d1e12b8da94b2aa7b5
nsw233 <nathanielweinman@aol.com>
2011-05-04 17:34:35 -0400

    hi

--- Python/addn.py
+++ Python/addn.py
@@ -1,14 +1,14 @@
 from parakeet import GPU
 from numpy import *
 
-@GPU
+#@GPU
 def mult9(x):
 #  y = mean(x)
 #  x = x * 2
 #  x = x + 4
   return x * 9
 
-@GPU
+#@GPU
 def addxy(x,y,z):
   q = mult9(x)
   return q * y + z
@@ -20,7 +20,7 @@ def whiletest(x):
     a = True
   return x
 
-#@GPU
+@GPU
 def sumtest(x):
   return sum(x)
 
@@ -28,7 +28,7 @@ x = array([1,2,3,4,5,6,7,8,9,10,11,12,13,14],dtype = int32)
 y = array([3,1,4,1,5,9,3,1,4,1,5,9,3,1],dtype = int32)
 z = array([[1,2,3],[4,5,6],[7,8,9]],dtype = int32)
 f = array([1.2, 2.5,3.4,1.2,5.3,1.5,9,8,7,6,1,2,3],dtype = float32)
-print mult9(x)
-print addxy(x,y,x)
+#print mult9(x)
+#print addxy(x,y,x)
 #print whiletest(x)
-#print sumtest(z)
+print sumtest(z)
