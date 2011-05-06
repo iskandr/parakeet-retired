f2747c8b1e9eba7ba80189c5ea291522d0b4630e
nsw233 <nathanielweinman@aol.com>
2011-05-05 23:20:01 -0400

    scalar return and while loops working

--- Python/addn.py
+++ Python/addn.py
@@ -2,15 +2,41 @@ from numpy import *
 #from functools import * 
 from parakeet import GPU
 
-@GPU
+#@GPU
 def sqr_dist(x,y): 
+    
     return sum((x-y) * (x-y))
 
-@GPU
+#@GPU
 def minidx(C,x): 
     return mean(map(partial(sqr_dist, x), C))
 
 x = array([1,2,3,4,5,6,7,8,9,10],dtype = int32)
 y = array([3,1,4,1,5,9,2,6,5,3],dtype = int32)
 
-print minidx(x,y)
+#print sqr_dist(x,2)
+#print minidx(x,y)
+
+@GPU
+def add2(x,y):
+  return x + y
+
+@GPU
+def whileTestScalar(x):
+  while x > 0:
+    x = x - add2(1,2)
+  return x
+
+print whileTestScalar(1.2)
+
+def whileTest(x,y):
+  converged = False
+  while not converged:
+    z = sqr_dist(x,y)
+    converged = True
+  return z
+#  while x > 0:
+#    x = x - 1
+#  return x
+
+
