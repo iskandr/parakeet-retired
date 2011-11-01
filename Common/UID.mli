253cab8213c3b19b06ea2e1b45895052babe1a09
Alex Rubinsteyn <alexr@cs.nyu.edu>
2011-11-01 02:08:14 -0400

    fixed up UID, ID

--- Common/UID.mli
+++ Common/UID.mli
@@ -3,16 +3,18 @@ module type S = sig
   type t 
   val to_str : t -> string
   
-  type uid = t  
-  module Set : module type of Set.Make(struct type t = uid let compare = compare end)
-  module Map : module type of Map.Make(struct type t = uid let compare = compare end)
+  module Set : Set.S with type elt = t  
+  module Map : Map.S with type key = t 
 
   val gen : unit -> t
    
   (* takes a list of ids, returns a mapping of id -> fresh id *)
-  val map_fresh : t list -> t list 
+  val map_fresh : t list -> t Map.t  
+  
   val gen_fresh_list : int -> t list
   val gen_fresh_array : int -> t array  
+  
+  val of_int : int -> t 
 end
 
 module Make(A : sig val to_str : int -> string end) : S  
