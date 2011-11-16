a348a5d5c9e15f10f7338cf07cf331567deadcdc
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2011-11-15 20:47:20 -0500

    added new backend signature

---
+++ Runtime/Backend.mli
@@ -0,0 +1,19 @@
+type data = Ptr.t Value.t
+
+module type S = sig 
+	val memspace_id : MemId.t  
+   
+	val map :  axes:int list -> fn:SSA.fn -> fixed:(data list) -> 
+  	  data list -> data list
+
+	val reduce : axes:int list -> fn:SSA.fn -> fixed:(data list) -> 
+  	  ?init:data list -> data list -> data list
+
+	val scan : axes:int list -> fn:SSA.fn -> fixed:(data list) -> 
+  	  ?init:data list -> data list -> data list
+
+	val all_pairs : axes:int list -> fn:SSA.fn -> fixed:(data list) -> 
+  	  data -> data -> data list
+
+	val array_op : Prim.array_op -> data list -> data list
+end 
