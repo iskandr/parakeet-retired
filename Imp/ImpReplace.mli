3994a8a70ec2890ef9c3e70be16c5ce2de15027c
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2011-12-07 20:37:24 -0500

    out ImpReplace back in source tree

---
+++ Imp/ImpReplace.mli
@@ -0,0 +1,9 @@
+
+val apply_exp_map : Imp.exp_node ID.Map.t  -> Imp.exp_node -> Imp.exp_node 
+val apply_exp_map_to_stmt : Imp.exp_node ID.Map.t  -> Imp.stmt -> Imp.stmt 
+  
+val apply_id_map : ID.t ID.Map.t -> Imp.exp_node -> Imp.exp_node  
+val apply_id_map_to_stmt : ID.t ID.Map.t -> Imp.stmt -> Imp.stmt
+
+(* same function, fresh IDs *) 
+val fresh_fn : Imp.fn -> Imp.fn 
