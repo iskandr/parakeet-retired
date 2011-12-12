6d071ca48d91e266c49b8e9d68ede58c9cf3a963
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2011-12-12 18:01:50 -0500

    fixed pattern match in GenericValue_to_Value

--- Runtime/LLVM/GenericValue_to_Value.ml
+++ Runtime/LLVM/GenericValue_to_Value.ml
@@ -3,17 +3,17 @@ open Llvm
 
 let context = global_context ()
 
-let generic_to_parnum g_val impt = function match impt with
-  | ImpType.bool_t -> as_int g_val <> 0
-  | ImpType.char_t -> Char.chr (as_int g_val)
-  | ImpType.int16_t -> as_int g_val
-  | ImpType.int32_t -> as_int32 g_val
-  | ImpType.int64_t -> as_int64 g_val
-  | ImpType.float32_t -> as_float (float_type context) g_val
-  | ImpType.float64_t -> as_float (double_type context) g_val
+let generic_to_parnum (g_val:GenericValue.t) (impt:ImpType.t) : ParNum.t = match impt with
+  | Type.BoolT -> as_int g_val <> 0
+  | Type.CharT -> Char.chr (as_int g_val)
+  | Type.Int16T -> as_int g_val
+  | Type.Int32T -> as_int32 g_val
+  | Type.Int64T -> as_int64 g_val
+  | Type.Float32T -> as_float (float_type context) g_val
+  | Type.Float64T -> as_float (double_type context) g_val
   | _ -> assert false
 
-let generic_to_imp g_val = function 
+let generic_to_imp (g_val:GenericValue.t) = function 
   | ImpType.ScalarT t -> generic_to_parnum g_val t
   | ImpType.ArrayT _ -> assert false
   | _ -> assert false
