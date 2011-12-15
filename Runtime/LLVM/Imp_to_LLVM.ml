8ff2ddc997e8232e565a831cde0ab7ff6f464c78
Nathaniel Weinman <nsw233@nyu.edu>
2011-12-14 23:22:31 -0500

    Updated imp to llvm code, should have the bare minimum required to work

--- Runtime/LLVM/Imp_to_LLVM.ml
+++ Runtime/LLVM/Imp_to_LLVM.ml
@@ -17,50 +17,133 @@ let array_field_to_idx = function
   | FrozenIdx -> 2
 
 
+let context = Llvm.global_context ()
+let the_module = Llvm.create_module context "my_module"
+let named_values : (ID.t, llvalue) Hashtbl.t = Hashtbl.create 10
+type function_record = { vars: ID.t list;
+                         types: Llvm.lltype list;
+                         builder: Llvm.llbuilder}
 
-let named_values : (string, llvalue) Hashtbl.t = Hashtble.create 10 in
-  let rec compile_stmt_seq types body = 
-    match body with
-    | [] -> []
-    | head :: tail -> compile_stmt types head :: compile_stmt_seq types body
-  and rec compile_stmt types = function
-    | Imp.If (cond, t_b, f_b) -> assert false
-    | Imp.While exp b -> assert false
-    | Imp.Set id exp -> assert false
-      let val_ = compile_expr types exp in
-      let variable = try Hashtbl.find named_values name with
-      | Not_found -> raise (Error "unknown variable name " ^ id)
-      in
-      ignore(build_store val_ variable builder);
-      (* val_ *)
-    | Imp.SetIdx id idxs exp -> assert false
-    | Imp.SyncThreads -> assert false
-    | Imp.Comment str -> ()
-  and compile_expr types imp_expr = 
-    match imp_expr with 
-    | Imp.Val val_node -> compile_val types val_node
-    | Imp.Op t op vals -> assert false
-    | Imp.Select t cond t_v f_v -> assert false
-    | Imp.Cast t value -> assert false
-    | Imp.Idx var idxs -> assert false
-    | Imp.DimSize var dim -> assert false
-    | Imp.FreezeDim _ -> assert false
-    | Imp.ArrayField ? ? -> assert false
-  and compile_val types imp_val =
-    match compile_val with
-    | Imp.Var id -> 
-      let v = try Hashtbl.find named_values name with
-      | Not_found -> raise (Error "unkown variable name")
+
+let create_llvm_vars inputs locals outputs = 
+  List.concat [inputs;locals;outputs]
+
+let create_llvm_types inputTypes localTypes outputTypes = 
+  let impTypes = List.concat [inputTypes;localTypes;outputTypes] in
+  List.map (fun impType -> ImpType_to_lltype.to_lltype impType) impTypes
+
+let codegen_proto llvmVars llvmTypes name =
+  let ft = Llvm.function_type Llvm.void llvmTypes in
+  let f = Llvm.declare_function name ft the_module in
+  let params = Array.to_list Llvm.params f in
+  List.iter2 (var param ->
+    let varName = ID.to_str var in
+    Llvm.set_value_name varName param;
+    Hashtbl.add named_values varName param
+  ) (llvmVars params);
+  f
+
+let create_argument_allocas theFunction llvmVars llvmTypes =
+  let params = Array.to_list Llvm.params theFunction in
+  List.iter2 (var param ->
+    let varName = ID.to_str var in
+    let alloca = Llvm.create_entry_block_alloca theFunction varName in
+    ignore(Llvm.build_store param alloca builder);
+    Hashtbl.add named_values varName alloca;
+  ) (llvmVars params)
+
+let init_compiled_fn llvmVars llvmTypes name builder =
+  Hashtbl.clear named_values
+  let theFunction = codegen_proto llvmVars llvmTypes name in
+  let bb = Llvm.append_block context "entry" theFunction in
+  Llvm.position_at_end bb builder;
+  try
+    create_argument_allocas theFunction llvmVars llvmTypes
+  with e ->
+    Llvm.delete_function the_function;
+    raise e
+  bb;
+
+let rec compile_stmt_seq fnInfo currBB = 
+  match body with
+  | [] -> currBB
+  | head :: tail ->
+    let newBB = compile_stmt fnInfo currBB head in
+    compoile_stmt_seq fnInfo newBB tail
+and rec compile_stmt fnInfo currBB = function
+  | Imp.Set (id, exp) ->
+    let rhs = compile_expr fnInfo exp in
+    (* Alex: why don't I need a begin/end here? *)
+    let variable = try Hashtbl.find named_values (ID.to_str id) with
+    | Not_found -> raise (Error "unknown variable name " ^ (ID.to_str id))
+    in
+    ignore(Llvm.build_store rhs variable fnInfo.builder);
+    currBB
+  | _ -> assert false
+and compile_expr fnInfo impExpr = match impExpr with
+  | Imp.Val (valNode) -> compile_val valNode
+  | Imp.Op (t, op, vals) ->
+  begin
+    match op, vals with
+    | Prim.Add [ lVal; rVal ] ->
+    let lhs = compile_val lVal in
+    let rhs = compile_val rVal in
+    begin
+      match t with
+      | Type.Int16T
+      | Type.Int32T
+      | Type.Int64T -> Llvm.build_add lhs rhs "addtmp" fnInfo.builder
+      | Type.Float32T
+      | Type.Float64T -> Llvm.build_fadd lhs rhs "addftmp" fnInfo.builder
+    end;
+    | Prim.Sub [ lVal; rVal ] ->
+    let lhs = compile_val lVal in
+    let rhs = compile_val rVal in
+    begin
+      match t with
+      | Type.Int16T
+      | Type.Int32T
+      | Type.Int64T -> Llvm.build_sub lhs rhs "subtmp" fnInfo.builder
+      | Type.Float32T
+      | Type.Float64T -> Llvm.build_fsub lhs rhs "subftmp" fnInfo.builder
+    end;
+    | Prim.mul [ lVal; rVal ] ->
+    let lhs = compile_val lVal in
+    let rhs = compile_val rVal in
+    begin
+      match t with
+      | Type.Int16T
+      | Type.Int32T
+      | Type.Int64T -> Llvm.build_mul lhs rhs "multmp" fnInfo.builder
+      | Type.Float32T
+      | Type.Float64T -> Llvm.build_fmul lhs rhs "mulftmp" fnInfo.builder
+    end;
+    | Prim.add [ lVal; rVal ] ->
+    let lhs = compile_val lVal in
+    let rhs = compile_val rVal in
+    Llvm.build_udiv lhs rhs "addudiv" fnInfo.builder
+  end;
+  _ -> assert false
+and compile impVal = match impVal with
+  | Imp.Var (id) ->
+    begin (* Alex: begin necessary here? *)
+      let v = try Hashtbl.find named_values (ID.to_str id) with
+      | Not_found -> raise (Error "unknown variable name")
       in
       v
-    | Imp.Const const -> Value_to_GenereicValue.to_llvm const
-    | CudaInfo _ -> assert false
+    end;
+  | Imp.Const (const) -> Value_to_llvalue.to_llvm const
+  | _ -> assert false
 
-
-let compile_fn fn = 
+let compile_fn fn =
+  let builder = Llvm.builder context in
   let inputTypes = List.map (fun id -> ID.Map.find id fn.types) fn.input_ids in
-  let outputTypes = List.map (fun id -> ID.Map.find id fn.types) fn.output_ids in  
-  let llvm_fn = init_compiled_fn inputTypes outputTypes in 
-  let first_basic_block = compile_stmt_seq llvm_fn fn.types fn.body in
-  (* TODO: set first_basic_block as entry into llvm_fn *)  
-  llvm_fn 
+  let localTypes = List.map (fun id -> ID.Map.find id fn.types) fn.local_ids in
+  let outputTypes = List.map (fun id -> ID.Map.find id fn.types) fn.output_ids in
+  
+  let llvmVars = create_llvm_vars fn.input_ids fn.local_ids fn.output_ids in
+  let llvmTypes = create_llvm_types inputTypes localTypes outputTypes in
+  let fnInfo = { vars = llvmVars; types = llvmTypes; builder = builder } in
+  let initBasicBlock = init_compiled_fn llvmVars llvmTypes (FnId.to_str fn.id) builder in
+  let firstBasicBlock = compile_stmt_seq fnInfo initBasicBlock in
+  firstBasicBlock
