ac4257052a56255220e21b61cf9588038a54998a
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2011-12-22 16:10:57 -0500

    modified identity function test

--- Python/test_id.py
+++ Python/test_id.py
@@ -5,19 +5,16 @@ from parakeet import GPU
 def identity(x):
   return x
 
-multi_test = reshape(arange(12, dtype=int32), (3,4))
-#multi_test = arange(12, dtype=int32)
+def test_scalar_id():
+  assert 3 == identity(3)
 
-print "Input:", multi_test
-print "Input Shape:", shape(multi_test)
-print "Input Type:", multi_test.dtype
-res =  identity(multi_test)
-print "Output: ", res
-print "Output Shape:", shape(res)
-print "Output Type:", res.dtype
-#import unittest
-
-#class TestIdent(unittest.TestCase):
-#  def testMethod(self):
-#    self.assertTrue(all(id(multi_test) == multi_test), "identity function broken on 2d data")
-
+def test_array_id():
+  multi_test = reshape(arange(12, dtype=int32), (3,4))
+  print "Input:", multi_test
+  print "Input Shape:", shape(multi_test)
+  print "Input Type:", multi_test.dtype
+  res =  identity(multi_test)
+  print "Output: ", res
+  print "Output Shape:", shape(res)
+  print "Output Type:", res.dtype
+  assert res == multi_test
