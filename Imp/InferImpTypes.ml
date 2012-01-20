759ae8e1373aa1e745e230c255accf3d0885fb1f
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2012-01-20 15:23:14 -0500

    fixing infer imp types

--- Imp/InferImpTypes.ml
+++ Imp/InferImpTypes.ml
@@ -4,30 +4,28 @@ open SSA
 
 type tenv = ImpType.t ID.Map.t  
 
+let scalar_imp_type t = ImpType.ScalarT (Type.elt_type t)
+
 let infer_value (tenv:tenv) {value; value_type} : ImpType.t =
-  if Type.is_scalar value_type then 
-    ImpType.ScalarT (Type.elt_type value_type)
+  if Type.is_scalar value_type then scalar_imp_type value_type 
   else match value with 
     | SSA.Var id -> 
-	    if not $ ID.Map.mem id tenv then 
+      if not $ ID.Map.mem id tenv then 
         failwith $ "ID not found: " ^ ID.to_str id
       else  
-	      ID.Map.find id tenv
-     
+        ID.Map.find id tenv   
     | Num n ->  ImpType.ScalarT (ParNum.type_of n)
     | other -> failwith $ "[ImpInferTypes] invalid value: " ^ (SSA.value_to_str other)  
 
-let infer_exp (tenv:tenv) {exp} : ImpType.t list = 
-  match exp with 
+let infer_exp (tenv:tenv) {exp; exp_types} : ImpType.t list = 
+  if List.for_all Type.is_scalar exp_types then List.map scalar_imp_type exp_types 
+  else  match exp with 
   | Values vs -> List.map (infer_value tenv) vs  
   | App _ -> failwith "[InferImpTypes] Unexpected untyped function application" 
   | Arr vs -> failwith "[InferImpTypes] Array literals not yet implemented"
-  | Cast (t, v) -> 
-    (* assume casts are always between scalar types *) 
-    assert (Type.is_scalar t); 
-    [ImpType.ScalarT (Type.elt_type t)]
+  | Cast (t, v) -> failwith "[InferImpTypes] Unexpected non-scalar cast"
   | Call (fnId, args) -> failwith "[InferImpTypes] Typed function calls not implemented" 
-  | PrimApp (prim, args) -> failwith "[InferImpTypes] Primitives not implemented"   
+  | PrimApp (prim, args) -> failwith "[InferImpTypes] Unexpected non-scalar primitive"  
   | Adverb (adverb, closure, adverb_args) -> failwith "[InferImpTypes] adverbs not implemented"
    
 let rec infer_stmt (tenv:tenv) {stmt} = match stmt with 
