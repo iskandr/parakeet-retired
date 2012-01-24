a8d30f67aa2843f9dbaf4bee7f8740633ed1e866
Eric Hielscher <erik@sausage.nyu.edu>
2012-01-24 10:33:40 -0500

    Randomness.

--- Runtime/LLVM/Imp_to_LLVM.ml
+++ Runtime/LLVM/Imp_to_LLVM.ml
@@ -37,7 +37,7 @@ let create_fn_info (fnId:FnId.t) = {
 }
 
 let codegen_proto (fnInfo:fn_info) (llvmVars : ID.t list)
-                  (llvmTypes : Llvm.lltype array)  : Llvm.llvalue =
+                  (llvmTypes : Llvm.lltype array) : Llvm.llvalue =
   let ft = Llvm.function_type void_t llvmTypes in
   let f = Llvm.declare_function fnInfo.name ft global_module in
   let params = Array.to_list  (Llvm.params f) in
@@ -91,11 +91,11 @@ let compile_val (fnInfo:fn_info) (impVal:Imp.value_node) : Llvm.llvalue =
   | _ -> assert false
 
 (* Change to function? *)
-let compile_expr fnInfo (impExpr:Imp.exp_node) = 
+let compile_expr fnInfo (impExpr:Imp.exp_node) =
   match impExpr.exp with
-  | Imp.Val valNode -> compile_val fnInfo valNode 
+  | Imp.Val valNode -> compile_val fnInfo valNode
   | Imp.Op (t, op, vals) ->
-      let vals' =  List.map (compile_val fnInfo) vals in 
+      let vals' =  List.map (compile_val fnInfo) vals in
       ( match op, vals' with
         | Prim.Add, [ x; y ] ->
           begin match t with
@@ -125,28 +125,28 @@ let compile_expr fnInfo (impExpr:Imp.exp_node) =
       )
  | _ -> assert false
 
-let rec compile_stmt_seq fnInfo currBB = function 
+let rec compile_stmt_seq fnInfo currBB = function
   | [] -> currBB
   | head :: tail ->
     let newBB = compile_stmt fnInfo currBB head in
     compile_stmt_seq fnInfo newBB tail
-    
-and compile_stmt fnInfo currBB stmt = match stmt with 
-  | Imp.Set (id, exp) ->
 
+and compile_stmt fnInfo currBB stmt = match stmt with
+  | Imp.Set (id, exp) ->
     let rhs = compile_expr fnInfo exp in
     let variable = try Hashtbl.find fnInfo.named_values (ID.to_str id) with
     | Not_found -> failwith  ("unknown variable name " ^ (ID.to_str id))
     in
-    let instr = Llvm.build_store rhs variable fnInfo.builder in 
-    print_endline $ "generating store for " ^  (Imp.stmt_to_str stmt); 
-    dump_value rhs; 
-    dump_value instr; 
+    let instr = Llvm.build_store rhs variable fnInfo.builder in
+    print_endline $ "generating store for " ^ (Imp.stmt_to_str stmt);
+    dump_value rhs;
+    dump_value instr;
     currBB
   | _ -> assert false
 
 let init_compiled_fn fnInfo ~local_ids ~local_types ~input_ids ~input_types =
-  let theFunction = codegen_proto fnInfo input_ids (Array.of_list input_types)  in
+  let theFunction = codegen_proto fnInfo input_ids (Array.of_list input_types)
+  in
   let bb = Llvm.append_block context "entry" theFunction in
   Llvm.position_at_end bb fnInfo.builder;
   begin try
@@ -161,8 +161,8 @@ let init_compiled_fn fnInfo ~local_ids ~local_types ~input_ids ~input_types =
 let add_output_return fnInfo output_ids = 
   if List.length output_ids == 1 then
     let output_id = List.hd output_ids in
-    let variable = try Hashtbl.find fnInfo.named_values (ID.to_str output_id)
-      with
+    let variable =
+      try Hashtbl.find fnInfo.named_values (ID.to_str output_id) with
       | Not_found -> failwith ("unknown variable name " ^ (ID.to_str output_id))
     in
 		let tempName = (ID.to_str output_id) ^ "_value" in
@@ -180,23 +180,23 @@ let compile_fn (fn : Imp.fn) : Llvm.llvalue =
     List.map (fun id -> ID.Map.find id fn.types) fn.output_ids in
   (* concat true locals and local version of output vars *) 
   let localVarIds : ID.t list = fn.local_ids @ fn.output_ids in
-  let impTypes = localImpTypes @ outputImpTypes in 
-  let localTypes : Llvm.lltype list = 
+  let impTypes = localImpTypes @ outputImpTypes in
+  let localTypes : Llvm.lltype list =
     List.map (fun impType -> ImpType_to_lltype.to_lltype impType) impTypes
-  in 
-  let inputTypes : Llvm.lltype list = 
-    List.map (fun impType -> ImpType_to_lltype.to_lltype impType) inputImpTypes 
   in
-  let fnInfo = create_fn_info fn.id in  
-  let llvmFn : Llvm.llvalue = 
-    init_compiled_fn 
+  let inputTypes : Llvm.lltype list =
+    List.map (fun impType -> ImpType_to_lltype.to_lltype impType) inputImpTypes
+  in
+  let fnInfo = create_fn_info fn.id in
+  let llvmFn : Llvm.llvalue =
+    init_compiled_fn
       fnInfo
       ~input_ids:fn.input_ids
-      ~input_types:inputTypes  
+      ~input_types:inputTypes
       ~local_ids:localVarIds
       ~local_types:localTypes
-  in 
-  let initBasicBlock : Llvm.llbasicblock = Llvm.entry_block llvmFn in 
+  in
+  let initBasicBlock : Llvm.llbasicblock = Llvm.entry_block llvmFn in
   let _ : Llvm.llbasicblock = compile_stmt_seq fnInfo initBasicBlock fn.body in
   add_output_return fnInfo fn.output_ids;
-  llvmFn 
+  llvmFn
--- Runtime/LLVM/LLVM_Types.ml
+++ Runtime/LLVM/LLVM_Types.ml
@@ -1,16 +1,16 @@
-open Llvm 
-let context : llcontext  = global_context ()
+open Llvm
+let context : llcontext = global_context ()
 
-let void_t : lltype = void_type context 
+let void_t : lltype = void_type context
 
-let int16_t : lltype = i16_type context 
-let int32_t : lltype  = i32_type context
-let int64_t : lltype = i64_type context 
+let int16_t : lltype = i16_type context
+let int32_t : lltype = i32_type context
+let int64_t : lltype = i64_type context
 let float32_t : lltype = float_type context
-let float64_t : lltype = double_type context 
+let float64_t : lltype = double_type context
 
 let int16_ptr_t = Llvm.pointer_type int16_t
-let int32_ptr_t =  Llvm.pointer_type int32_t 
-let int64_ptr_t = Llvm.pointer_type int64_t 
+let int32_ptr_t = Llvm.pointer_type int32_t
+let int64_ptr_t = Llvm.pointer_type int64_t
 let float32_ptr_t = Llvm.pointer_type float32_t
-let float64_ptr_t = Llvm.pointer_type float64_t 
+let float64_ptr_t = Llvm.pointer_type float64_t
