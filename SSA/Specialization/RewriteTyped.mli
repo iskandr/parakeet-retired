1c7bd320df6ddf579d4e63138c2d15bc8f1bb1e3
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2012-01-25 12:44:04 -0500

    fixed scalar upcasting in rewrite typed

--- SSA/Specialization/RewriteTyped.mli
+++ SSA/Specialization/RewriteTyped.mli
@@ -4,4 +4,6 @@ val rewrite_typed :
       tenv : (ID.t, Type.t) Hashtbl.t -> 
       closureEnv : CollectPartialApps.closure_env ->
       specializer : (value -> Signature.t -> fn) ->
-      output_arity : (value -> int) ->  fn:fn -> fn 
+      output_arity : (value -> int) ->  
+      fn:fn -> 
+      fn 
