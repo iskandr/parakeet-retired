d81be52c46a00f47edbfd6ace3a2c45c1d3d421e
Eric Hielscher <erik@sausage.nyu.edu>
2012-01-25 13:56:08 -0500

    Added conditional test.

---
+++ Python/test_cond.py
@@ -0,0 +1,26 @@
+from numpy import *
+from parakeet import PAR
+import sys
+
+@PAR 
+def simple_if(x):
+  a = 0
+  if x:
+    a = 1
+  else:
+    a = 0
+  return a
+
+def test_if():
+  print "Testing If"
+  sys.stdout.flush()
+  x = simple_if(1)
+  print "Expected 1, got", x
+  assert 1 == x
+  y = simple_if(0)
+  print "Expected 0, got", y
+  assert 0 == y
+
+if __name__ == '__main__':
+  test_if()
+
