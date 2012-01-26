0006ac02f6a3fe1dfc84e9ec5692dc93c01e62a6
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2012-01-26 16:31:03 -0500

    prims

--- FrontEnd/ExternalLibNames.ml
+++ FrontEnd/ExternalLibNames.ml
@@ -1,55 +1,56 @@
 
-open Base 
+open Base
 open Prim
 
 let prim_names =  Hashtbl.of_list [
     "+", ScalarOp Add;
-    "-", ScalarOp Sub; 
+    "-", ScalarOp Sub;
     "/", ScalarOp Div;
-    "*", ScalarOp Mult; 
-    "mod", ScalarOp Mod; 
+    "*", ScalarOp Mult;
+    "mod", ScalarOp Mod;
     "max", ScalarOp Max;
-    "min", ScalarOp Min; 
-    
-    "eq", ScalarOp Eq; 
-    "neq", ScalarOp Neq; 
-    "<", ScalarOp Lt; 
+    "min", ScalarOp Min;
+
+    "eq", ScalarOp Eq;
+    "neq", ScalarOp Neq;
+    "<", ScalarOp Lt;
     "<=", ScalarOp Lte;
-    ">", ScalarOp Gt; 
-    ">=", ScalarOp Gte; 
-            
+    ">", ScalarOp Gt;
+    ">=", ScalarOp Gte;
+
     "sqrt", ScalarOp Sqrt;
     "ln", ScalarOp Ln;
-    "exp", ScalarOp Exp;   
-    "pow", ScalarOp Pow; 
+    "exp", ScalarOp Exp;
+    "pow", ScalarOp Pow;
     "log", ScalarOp Log;
-    
-    "not", ScalarOp Not; 
-    "neg", ScalarOp Neg; 
-    "sign", ScalarOp Sign; 
-    
+
+    "not", ScalarOp Not;
+    "neg", ScalarOp Neg;
+    "sign", ScalarOp Sign;
+
     "reciprocal", ScalarOp Reciprocal;
-    "floor", ScalarOp Floor; 
+    "floor", ScalarOp Floor;
     "ceil", ScalarOp Ceil;
     "abs", ScalarOp Abs;
-      
+
     "select", ScalarOp Select;
-    "map", Adverb Map; 
-    "each-left", Adverb EachLeft; 
+    "map", Adverb Map;
+    "each-left", Adverb EachLeft;
     "each-right", Adverb EachRight;
-    "allpairs", Adverb AllPairs; 
-    "scan", Adverb Scan;            
+    "allpairs", Adverb AllPairs;
+    "scan", Adverb Scan;
     "reduce", Adverb Reduce;
-       
-    "where", ArrayOp Where; 
-    "enlist", ArrayOp Enlist; 
-    "index",  ArrayOp Index;   
+
+    "where", ArrayOp Where;
+    "enlist", ArrayOp Enlist;
+    "index",  ArrayOp Index;
     "range", ArrayOp Til;
-    "concat", ArrayOp Concat; 
-]   
-                
+    "concat", ArrayOp Concat;
+    "slice", ArrayOp Slice;
+]
+
 let get_prim str = match Hashtbl.find_option prim_names str with
   | Some prim -> AST_Helpers.mk_prim_node prim
   | None ->
       if FnManager.have_untyped_function str then AST_Helpers.mk_var_node str
-      else failwith $ "Couldn't find Parakeet primitive named " ^ str  
+      else failwith $ "Couldn't find Parakeet primitive named " ^ str
