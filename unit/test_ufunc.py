ad3c4159b58c66d80c1f161e84276ce29649922d
Nathaniel Weinman <nsw233@nyu.edu>
2012-02-24 21:32:06 -0500

    ufunc stuff 2

---
+++ unit/test_ufunc.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import unit
+from unit import parakeet
+from parakeet import PAR
+import numpy as np
+
+@PAR
+def addReduce(x):
+  return np.add.reduce(x)
+
+@PAR
+def addReduceAxis(x):
+  return np.add.reduce(x,1)
+
+def test_reduce():
+  x = np.array(([1,2,3],[4,5,6]))
+  out = addReduce(x)
+  e_out = np.array(([5,7,9]))
+  print "Expected ", e_out, " got: ", out
+  assert np.all(out == e_out)
+  out = addReduceAxis(x)
+  e_out = np.array(([6,15]))
+  print "Expected ", e_out, " got: ", out
+  assert np.all(out == e_out)
+
+if __name__ == '__main__':
+  test_reduce()
