263a0f72c1038b518c42ba10e74fdad3dd70cd5c
Eric Hielscher <erik@sausage.nyu.edu>
2012-02-29 14:34:51 -0500

    Fixed multi-dimensional reductions.

--- Common/Shape.ml
+++ Common/Shape.ml
@@ -10,12 +10,12 @@
 open Base
 open Bigarray
 
-type t = int array 
+type t = int array
 
-let create n = Array.create n 0 
+let create n = Array.create n 0
 
 let get shape idx = shape.(idx)
-  
+
 let set shape idx v = shape.(idx) <- v
 
 let rank shape = Array.length shape
@@ -23,20 +23,20 @@ let rank shape = Array.length shape
 let scalar_shape = create 0
 
 let to_str s =
-  let r = rank s in   
-  let b = Buffer.create (r*3) in 
+  let r = rank s in
+  let b = Buffer.create (r*3) in
   Printf.bprintf b "[";
-  for i = 0 to r - 1 do 
-    if i < r - 1 then 
-      Printf.bprintf b "%d, " (get s i) 
-    else 
+  for i = 0 to r - 1 do
+    if i < r - 1 then
+      Printf.bprintf b "%d, " (get s i)
+    else
       Printf.bprintf b "%d" (get s i)
-  done; 
+  done;
   Printf.bprintf b "]";
   Buffer.contents b
 
-let shape_list_to_str shapes = 
-      "{" ^ (String.concat "; " (List.map to_str shapes)) ^ "}"
+let shape_list_to_str shapes =
+  "{" ^ (String.concat "; " (List.map to_str shapes)) ^ "}"
 
 let of_list l =
   let n = List.length l in
@@ -47,35 +47,35 @@ let of_list l =
 (* the product of the elements in a shape vector is the number of elements
    in an array
 *)
-let nelts shape = Array.fold_left (fun acc x -> acc * x) 1 shape 
+let nelts shape = Array.fold_left (fun acc x -> acc * x) 1 shape
 
 let nbytes shape = 4 * rank shape
 
 let eq s1 s2 =
-  (rank s1 = rank s2) && 
+  (rank s1 = rank s2) &&
   (
     let are_same = ref true in
     for i = 0 to rank s1 - 1 do
-      are_same := !are_same && get s1 i = get s2 i 
+      are_same := !are_same && get s1 i = get s2 i
     done;
     !are_same
   )
 
-(* shape s1 is a subshape of s2 if s1 is a scalar or 
-   there is a rightmost subarray of s2 which is equal to s1 
+(* shape s1 is a subshape of s2 if s1 is a scalar or
+   there is a rightmost subarray of s2 which is equal to s1
 *)
-let is_subshape s1 s2 = 
+let is_subshape s1 s2 =
   let r1, r2 = rank s1, rank s2 in
-  if r1 > r2 then false 
-  else if r1 = 0 then true 
-  else  
-    let acc = ref true in 
-    for i = 0 to r1 - 1 do 
+  if r1 > r2 then false
+  else if r1 = 0 then true
+  else
+    let acc = ref true in
+    for i = 0 to r1 - 1 do
       acc := !acc && get s1 i = get s2 (r2 - r1 + i)
-    done; 
+    done;
     !acc
 
-(* max_shape is a partial function since there can be no max between 
+(* max_shape is a partial function since there can be no max between
    unrelated shapes
 *)
 let max_shape s1 s2 =
@@ -85,68 +85,68 @@ let max_shape s1 s2 =
 
 (* given a list of shapes, return the maximal element,
    or None if the shapes are unrelated
-*) 
+*)
 let max_shape_list lst =
-  let aux maybeS1 s2 = match maybeS1 with 
+  let aux maybeS1 s2 = match maybeS1 with
     | None -> None
-    | Some s1 -> max_shape s1 s2 
-  in  
+    | Some s1 -> max_shape s1 s2
+  in
   List.fold_left aux (Some scalar_shape) lst
 
-let peel ?(axes=[0]) shape = 
+let peel ?(axes=[0]) shape =
   let n = rank shape in
   let num_axes = List.length axes in
-  let diff = n - num_axes in 
+  let diff = n - num_axes in
   if diff <= 0 then scalar_shape
   else
-    let all_axes =  List.til n in 
-    let kept_axes = List.filter (fun x -> not $ List.mem x axes) all_axes in  
-    let shape' = create diff in 
-    let idx = ref 0 in 
-    let set_dim old_axis = 
-      let old_dim = get shape old_axis in 
-      set shape' !idx old_dim; 
+    let all_axes =  List.til n in
+    let kept_axes = List.filter (fun x -> not $ List.mem x axes) all_axes in
+    let shape' = create diff in
+    let idx = ref 0 in
+    let set_dim old_axis =
+      let old_dim = get shape old_axis in
+      set shape' !idx old_dim;
       idx := !idx + 1
-    in 
-    List.iter set_dim kept_axes;   
+    in
+    List.iter set_dim kept_axes;
     shape'
 
-(* peel the maximal shapes in a list, leaving the rest unchanged *) 
-let split_nested_shapes shapes = 
-  let maxShape = Option.get (max_shape_list shapes) in 
-  if rank maxShape = 0 then 
-    failwith $ 
+(* peel the maximal shapes in a list, leaving the rest unchanged *)
+let split_nested_shapes shapes =
+  let maxShape = Option.get (max_shape_list shapes) in
+  if rank maxShape = 0 then
+    failwith $
       "Cannot split nested shapes when all shape arguments are scalars: " ^
       (shape_list_to_str shapes)
-      ; 
-  let peeler shape = 
-    if eq shape maxShape then peel shape else shape 
-  in 
-  get maxShape 0, List.map peeler shapes  
- 
-let append shape1 shape2 = 
-  let m = rank shape1 in 
-  let n = rank shape2 in 
-  let combined = create (m+n) in 
-  for i = 0 to m - 1 do 
-    set combined i (get shape1 i)
-  done; 
-  for i = m to m+n-1 do 
-    set combined i (get shape2 (i - m))
-  done; 
-  combined 
-  
-let append_dim dim shape = 
-  let n = rank shape in 
-  let shape' = create (n+1) in
-  set shape' 0 dim;  
-  for i = 0 to n - 1 do 
-    set shape' (i+1) (get shape i)
-  done; 
-  shape'  
+      ;
+  let peeler shape =
+    if eq shape maxShape then peel shape else shape
+  in
+  get maxShape 0, List.map peeler shapes
 
-let append_dims dims shape = 
-  append (of_list dims) shape 
+let append shape1 shape2 =
+  let m = rank shape1 in
+  let n = rank shape2 in
+  let combined = create (m+n) in
+  for i = 0 to m - 1 do
+    set combined i (get shape1 i)
+  done;
+  for i = m to m+n-1 do
+    set combined i (get shape2 (i - m))
+  done;
+  combined
+
+let append_dim dim shape =
+  let n = rank shape in
+  let shape' = create (n+1) in
+  set shape' 0 dim;
+  for i = 0 to n - 1 do
+    set shape' (i+1) (get shape i)
+  done;
+  shape'
+
+let append_dims dims shape =
+  append (of_list dims) shape
 
 let slice_shape inputShape dimsList =
   let n = rank inputShape in
@@ -185,4 +185,3 @@ let of_int32_c_array cArr =
     set shape i (Int32.to_int cArr.{i})
   done;
   shape
-
