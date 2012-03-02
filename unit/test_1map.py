11a8cb1a200e49fa5db079bfac4565783222569d
Eric Hielscher <erik@sausage.nyu.edu>
2012-03-02 17:08:07 -0500

    Added test_1map.py.

---
+++ unit/test_1map.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import numpy as np
+import parakeet, sys
+from parakeet import PAR
+
+@PAR 
+def implicit_map(x):
+  return x * 3
+
+def test_implicit_map():
+  print "Testing implicit maps" 
+  x = np.array(range(127), dtype=np.int32)
+  y = implicit_map(x)
+  y_original = implicit_map.call_original(x)
+  print "Python = %s, Parakeet = %s" % (y_original, y)
+  assert np.all(y_original == y)
+
+if __name__ == '__main__':
+  test_implicit_map()
+
