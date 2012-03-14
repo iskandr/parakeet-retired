c2826c8f820790c5d44eeb49be7c92b4b5a25c69
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2012-03-14 16:03:58 -0400

    matrix multiply getting closer

--- unit/test_matmult.py
+++ unit/test_matmult.py
@@ -14,11 +14,12 @@ def mm(X,Y):
   return parakeet.allpairs(dot, X, Y, axis=[0])
 
 def test_mm():
-  X = np.random.randn(1000, 1000)
-  Y = np.random.randn(1000, 1000)
-  parakeet_result = mm(X,Y.T)
+  X = np.array([[1,2,3,4,5], [6,7,8,9,10]], dtype='int32')
+  #X = np.random.randn(10, 10)
+  #Y = np.random.randn(10, 10)
+  parakeet_result = mm(X,X)
   print parakeet_result
-  np_result = np.dot(X,Y)
+  np_result = np.dot(X,X.T)
   print np_result
   assert np.sum( (parakeet_result - np_result) ** 2) <= 0.0001
 
