f80a7878336f9cbcc2728aa1acd0d33d65f21912
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2012-09-05 12:34:07 -0400

    adverbs adverbs everywhere

--- Common/Adverb.ml
+++ Common/Adverb.ml
@@ -1,73 +1,39 @@
-type t = Map | Reduce | Scan | AllPairs
+type t = Map | Reduce | Scan 
 
 
-type ('a, 'b, 'c) map_info = { 
-  map_fn : 'a; 
-  map_fixed_args : 'b; 
-  map_array_args : 'b; 
-  map_axes : 'c;   
+type ('a, 'b) init = 
+  | IniFn of 'a 
+  | InitValues of 'b 
+
+type ('a, 'b, 'c) adverb_info { 
+  adverb : t; 
+  adverb_fn : 'a; 
+  fixed_args : 'b;
+  array_args : 'b; 
+  axes : 'c; 
+  (* initialize scans and reductions either with value or function mapping
+     array element to acc 
+  *) 
+  init : ('a, 'b) init option; 
+  (* given two accumulators, combine them into a single new accumulator
+     if this function isn't given we can't parallelize reductions & scans 
+  *) 
+  combine_fn : 'a option;  
 }
 
-
-type 'a init = 
-  | InitFirstElement
-  | InitValues of 'a 
-
-type ('a, 'b, 'c) reduce_info = { 
-  reduce_fn : 'a; 
-  combiner : 'a;
-  reduce_fixed_args : 'b;
-  init : 'b init; 
-  reduce_array_args : 'b; 
-  reduce_axes : 'c;
-  keep_prefix : bool; 
-}
-
-type adverb = Map of map_info | Reduce of reduce_info
-
-let apply_to_map_fields info fn values axes =
-  { 
-    map_fn = fn info.map_adverb_fn;
-    map_fixed_args = values info.map_fixed_args; 
-    map_array_args = values info.map_array_args; 
-    map_axes = axes info.map_axes; 
-  }
-  
-let apply_to_reduce_fields info fn values axes = 
-  { 
-    reduce_fn = fn info.reduce_fn; 
-    init = (match info.init with 
-      | InitValues vs -> InitWithValues (values vs)
-      | InitFirstElement -> InitFirstElement)
-
-
-    reduce_fixed_args = values info.reduce_fixed_args; 
-    combiner = fn info.combiner; 
-    
-    
-  reduce_fixed_args : 'b;
-  init : 'b init; 
-  reduce_array_args : 'b; 
-  reduce_axes : 'c;
-  keep_prefix : bool; 
-    combine_fn = fn info.combine_fn; 
-    fixed_args = values info.fixed_args;
-    init = (match info.init with 
-      | InitWithValues vs -> InitWithValues (values vs)
-      | other -> other)
-    axes = axes info.axes;
-    array_args = values info.array_args; 
+let apply_to_init fn values = function 
+  | InitFn f -> InitFn (fn f)
+  | InitValues vs -> InitValues (values vs)
 
 let apply_to_fields info ~(fn:'a -> 'd) ~(values:'b -> 'e) ~(axes:'c -> 'f) =
-  match info with 
-    | Map map_info -> apply_to_map_fields map_info fn values axes 
-    | Reduce reduce_info -> apply_to_reduce_fields reduce_info fn values axes
-    | Scan scan_info -> apply_to_scan_fields scan_info fn values axes 
-
- {
-    adverb = info.adverb;
-
-  }
+  { info with  
+      fn = fn adverb.fn;
+      fixed_args = values adverb.fixed_args; 
+      array_args = values adverb.array_args; 
+      axes = axes adverb.axes; 
+      init = Option.map (apply_to_init fn values) adverb.init; 
+      combine_fn = Option.map fn adverb.combine_fn; 
+  }  
 
 let init_to_str values_to_str = function
 
--- Common/Adverb.mli
+++ Common/Adverb.mli
@@ -1,4 +1,26 @@
+type t = Map | Reduce | Scan 
 
+
+type ('a, 'b) init = 
+  | IniFn of 'a 
+  | InitValues of 'b 
+
+type ('a, 'b, 'c) adverb_info { 
+  adverb : t; 
+  fn : 'a; 
+  fixed_args : 'b;
+  array_args : 'b; 
+  axes : 'c; 
+  (* initialize scans and reductions either with value or function mapping
+     array element to acc 
+  *) 
+  init : ('a, 'b) init option; 
+  (* given two accumulators, combine them into a single new accumulator
+     if this function isn't given we can't parallelize reductions & scans 
+  *) 
+  combine_fn : 'a option;  
+}
+(*
 type ('a, 'b, 'c) map_info = { 
   map_fn : 'a; 
   map_fixed_args : 'b; 
@@ -7,22 +29,22 @@ type ('a, 'b, 'c) map_info = {
 }
 
 
-type 'a init = 
-  | InitFirstElement
-  | InitValues of 'a 
+type ('a, 'b) init = 
+  | IniFn of 'a 
+  | InitValues of 'b 
 
 type ('a, 'b, 'c) reduce_info = { 
   reduce_fn : 'a; 
-  combiner : 'a;
+  combine_fn : 'a;
   reduce_fixed_args : 'b;
-  init : 'b init; 
+  init : ('a, 'b) init; 
   reduce_array_args : 'b; 
   reduce_axes : 'c;
   keep_prefix : bool; 
 }
 
 type adverb = Map of map_info | Reduce of reduce_info
-
+*) 
 val apply_to_fields :
   ('a, 'b, 'c) info -> 
     fn:('a->'d) -> values:('b -> 'e) -> axes:('c -> 'f) -> 
